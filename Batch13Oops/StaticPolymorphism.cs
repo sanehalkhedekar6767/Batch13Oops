3a0b530e98867d916f68fd4932e05478b5ef28d3
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-10-03 21:50:26 +0530

    Static Polymorphism

---
+++ Batch13Oops/StaticPolymorphism.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch13Oops
+{
+    class MathOperation
+    {
+        //Method overloading or compile time polymorphism or early binding
+        //Method name same class also same but different parameters
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b) {
+            return a + b; 
+        }
+    }
+    internal class StaticPolymorphism
+    {
+        static void Main()
+        {
+            MathOperation obj = new MathOperation();
+
+            //calling overload methods
+            int m1 = obj.Add(2, 3);
+            double m2 = obj.Add(4, 2);
+
+            Console.WriteLine("Sum of integers: "+m1 );
+            Console.WriteLine("Sum of doubles: "+m2 );
+
+        }
+    }
+}
