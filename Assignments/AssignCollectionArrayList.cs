3227c5548e7b16752638366522594d9c71a5f20a
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-10-04 20:12:36 +0530

    Assignment on Collection: List and HashTable

---
+++ Assignments/AssignCollectionArrayList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class AssignCollectionArrayList
+    {
+        static void Main()
+        {
+            //creating array list
+            ArrayList array = new ArrayList();
+
+            array.Add(1);
+            array.Add(2);   
+            array.Add(3);   
+            array.Add(4);
+            array.Add(5);
+            array.Add(6);   
+            array.Add(7);
+            array.Add(8);
+            array.Add(9);
+            array.Add(10);
+            Console.WriteLine("Original array-------------->");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("After removing element from array-------------->");
+            array.Remove(3);
+            foreach (int item in array)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine("After clear operation on array-------------->");
+            array.Clear();
+            foreach (int item in array)
+            {
+                Console.WriteLine(item + " ");
+            }
+
+
+
+        }
+    }
+}
---
+++ Assignments/AssignCollectionHashTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class AssignCollectionHashTable
+    {
+        static void Main()
+        {
+            //creating Hashtable
+            Hashtable myHashTable = new Hashtable();
+
+            //adding key-value pairing to the hashtable
+            myHashTable.Add(1, "java");
+            myHashTable.Add(2, "c");
+            myHashTable.Add(3, "Phython");
+            myHashTable.Add(4, "C#");
+            myHashTable.Add(5, "Php");
+
+
+            //accessing the items although typecasting
+            string val1 = (string)myHashTable[1];
+            string val2 = (string)myHashTable[2];
+            string val3 = (string)myHashTable[3];
+            string val4 = (string)myHashTable[4];
+            string val5 = (string)myHashTable[5];
+
+            //display value
+            Console.WriteLine($"value1: {val1}");
+            Console.WriteLine($"value2: {val2}");
+            Console.WriteLine($"value3: {val3}");
+            Console.WriteLine($"value4: {val4}");
+            Console.WriteLine($"value4: {val5}");
+
+
+            //removing items from hashtable
+            myHashTable.Remove(4);
+            myHashTable.Remove(3);
+
+            //deleting elements from hashtable
+            myHashTable.Clear();
+
+          
+        }
+    }
+}
