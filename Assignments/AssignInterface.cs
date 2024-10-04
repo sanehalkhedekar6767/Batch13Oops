0e7b43c31f543c0f29c48e831575a165617d9a60
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-10-04 20:13:07 +0530

    Assignment on Array

---
+++ Assignments/AssignInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal interface Iplayable
+    {
+        public void Play();
+    }
+    class VideoPlayer : Iplayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing video...");
+        }
+    }
+    class MusicPlayer : Iplayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing music...");
+        }
+    }
+
+    public class AssignInterface
+    {
+        static void Main()
+        {
+            Iplayable video = new VideoPlayer();
+            video.Play();
+            Iplayable music = new MusicPlayer();
+            music.Play();
+        }
+    }
+
+}
---
+++ Assignments/Assignment2Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Assignment2Array
+    {
+        public static String revString(String str)
+        {
+            String reverse = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            return reverse;
+        }
+        static void Main(String[] args)
+        {
+            String str = "CODEMIND TECHNOLOGY";
+            Console.WriteLine("Given string is: " + str);
+            Console.WriteLine("Reverse of string is: " + revString(str));
+        }
+    }
+}
+
